d6789016b73a64f34a4f87b6ac7f4999b38e35cc
Harjit Mavi <mavi.harjit@gmail.com>
2017-09-21 10:49:34 -0400

    updating .gitignore

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,16 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
+	
     public Application() {
         System.out.println ("Inside Application");
     }
